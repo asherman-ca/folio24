b075a45ca1f81c98235c0b8e8dea77e7e31cce83
Alex Sherman <asherman.ca@gmail.com>
2024-06-24 14:15:40 -0700

    resume data - update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button'
 import { Section } from '@/components/ui/section'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
+import { ProjectCard } from '@/components/ui/ProjectCard'
 
 export default async function Index() {
 	return (
@@ -70,7 +71,7 @@ export default async function Index() {
 				<h2 className='text-xl font-bold'>Work Experience</h2>
 				{RESUME_DATA.work.map((work) => (
 					<Card key={work.company}>
-						<CardHeader className='p-0'>
+						<CardHeader>
 							<div className='flex items-center justify-between gap-x-2 text-base'>
 								<h3 className='inline-flex items-center justify-center gap-x-1 font-semibold leading-none'>
 									<a className='hover:underline' href={work.link}>
@@ -98,11 +99,63 @@ export default async function Index() {
 								{work.title}
 							</h4>
 						</CardHeader>
-						<CardContent className='mt-2 text-xs print:text-[10px] p-0'>
+						<CardContent className='mt-2 text-xs print:text-[10px]'>
 							{work.description}
 						</CardContent>
 					</Card>
 				))}
+			</Section>
+			<Section>
+				<h2 className='text-xl font-bold'>Education</h2>
+				{RESUME_DATA.education.map((education) => {
+					return (
+						<Card key={education.school}>
+							<CardHeader>
+								<div className='flex items-center justify-between gap-x-2 text-base'>
+									<h3 className='font-semibold leading-none'>
+										{education.school}
+									</h3>
+									<div className='text-sm tabular-nums text-gray-500'>
+										{education.start} - {education.end}
+									</div>
+								</div>
+							</CardHeader>
+							<CardContent className='mt-2 print:text-[12px]'>
+								{education.degree}
+							</CardContent>
+						</Card>
+					)
+				})}
+			</Section>
+			<Section>
+				<h2 className='text-xl font-bold'>Skills</h2>
+				<div className='flex flex-wrap gap-1'>
+					{RESUME_DATA.skills.map((skill) => {
+						return (
+							<Badge className='print:text-[10px]' key={skill}>
+								{skill}
+							</Badge>
+						)
+					})}
+				</div>
+			</Section>
+			<Section>
+				<Section className='print-force-new-page scroll-mb-16'>
+					<h2 className='text-xl font-bold'>Projects</h2>
+					<div className='-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3'>
+						{RESUME_DATA.projects.map((project) => {
+							return (
+								<ProjectCard
+									key={project.title}
+									title={project.title}
+									description={project.description}
+									tags={project.techStack}
+									link={'link' in project ? project.link.href : undefined}
+								/>
+							)
+						})}
+					</div>
+				</Section>
 			</Section>
 		</main>
 	)
--- data/resume-data.tsx
+++ data/resume-data.tsx
@@ -38,208 +38,123 @@ export const RESUME_DATA = {
 	],
 	work: [
 		{
-			company: 'Film.io',
-			link: 'https://film.io',
+			company: 'Candace Crow Design',
+			link: 'https://candacecrowe.com/',
 			badges: ['Remote'],
-			title: 'Software Architect',
+			title: 'Lead Software Engineer',
 			start: '2024',
 			end: null,
-			description:
-				'Leading the development of the Film.io platform. Technologies: React, TypeScript, Node.js',
-		},
-		{
-			company: 'Parabol',
-			link: 'https://parabol.co',
-			badges: ['Remote'],
-			title: 'Senior Full Stack Developer',
-			start: '2021',
-			end: '2024',
 			description:
 				'Implemented new features, led a squad, improved code delivery process, and initiated migration from Emotion to Tailwind CSS. Technologies: React, TypeScript, GraphQL',
 		},
 		{
-			company: 'Clevertech',
-			link: 'https://clevertech.biz',
-			badges: ['Remote'],
-			title: 'Lead Android Developer → Full Stack Developer',
-			start: '2015',
+			company: 'Lyell Immunopharma',
+			link: 'https://lyell.com/',
+			badges: ['Hybrid'],
+			title: 'Team Leader',
+			start: '2021',
+			end: null,
+			description:
+				'Designed & implemented FE data solution for lyell.com, deployed & maintained IT automation workflows. Technologies: React, TypeScript, Node.js',
+		},
+		{
+			company: 'Nurx',
+			link: 'https://nurx.com',
+			badges: ['On Site'],
+			title: 'Software Engineer',
+			start: '2019',
 			end: '2021',
 			description:
-				'Created Android mobile apps and led teams for companies like Vision Media, DKMS, and AAA. Built a live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL',
+				'Implimented new features, performed database migrations & mutations, and implimented design patterns. Technologies: Ember, TypeScript, Node.js',
 		},
 		{
-			company: 'Jojo Mobile',
-			link: 'https://bsgroup.eu/',
-			badges: [],
-			title: 'Android Developer → Lead Android Developer',
-			start: '2012',
-			end: '2015',
+			company: 'MyOnlinePool',
+			link: 'https://myonlinepool.com',
+			badges: ['Remote'],
+			title: 'Lead Software Engineer',
+			start: '2017',
+			end: '2018',
 			description:
-				'Led the Android team and developed apps for major Polish companies like LOT, Polskie Radio, Agora, and PolskaPress',
+				'Created NFL-fantasy-sports web application. Implimented authentication features, NFL stats integrations and Stripe processing. Technologies: Rails, React, SQL',
 		},
 		{
-			company: 'Nokia Siemens Networks',
-			link: 'https://www.nokia.com',
-			badges: [],
-			title: 'C/C++ Developer',
-			start: '2010',
-			end: '2012',
-			description: 'Developed and tested software for LTE base stations',
+			company: 'App Academy',
+			link: 'https://www.appacademy.io/',
+			badges: ['On Site'],
+			title: 'Teaching Assistant',
+			start: '2017',
+			end: '2018',
+			description:
+				'Taught introductory Ruby and Javascript, assisted in platform debugging & feature development, and evaluated applicants. Technologies: Ruby, JavaScript, React, Rails',
 		},
 	],
 	skills: [
 		'JavaScript',
 		'TypeScript',
-		'React/Next.js/Remix',
+		'React/Next.js',
 		'Node.js',
 		'GraphQL',
-		'Relay',
-		'WebRTC',
+		'SQL',
+		'Ruby',
+		'Python',
 	],
 	projects: [
 		{
-			title: 'Parabol',
+			title: 'Deep Rock Builds',
 			techStack: [
-				'Full Stack Developer',
+				'Side Project',
+				'Next.js',
 				'TypeScript',
-				'React',
 				'Node.js',
-				'GraphQL',
+				'Postgres',
 			],
 			description:
-				'The Agile meeting co-pilot that delivers better meetings with less effort',
+				'Build sharing platform for the Deep Rock Galactic community.',
 			link: {
-				label: 'github.com',
-				href: 'https://parabol.co/',
+				label: 'deeprockbuilds.vercel.app',
+				href: 'https://deeprockbuilds.vercel.app/',
 			},
 		},
 		{
-			title: 'Evercast',
+			title: 'Liquify',
 			techStack: [
-				'Lead Frontend Developer',
+				'Side Project',
+				'Next.js',
 				'TypeScript',
-				'React',
 				'Node.js',
-				'GraphQL',
+				'Postgres',
 			],
 			description:
-				'Creative collaboration platform that combines video conferencing and HD media streaming',
+				'Crypto-futures trading testnet platform with real-time liquification events.',
 			link: {
-				label: 'evercast.us',
-				href: 'https://www.evercast.us/',
+				label: 'liquify-three.vercel.app',
+				href: 'https://liquify-three.vercel.app/',
 			},
 		},
 		{
-			title: 'Consultly',
+			title: 'Supper',
+			techStack: ['Side Project', 'TypeScript', 'Next.js', 'Postgres'],
+			description: 'A Job Search Tracker with AI CV Builder',
+			link: {
+				label: 'supper-eta.vercel.app/',
+				href: 'https://supper-eta.vercel.app/',
+			},
+		},
+		{
+			title: 'Chonkify',
 			techStack: [
 				'Side Project',
 				'TypeScript',
 				'Next.js',
-				'Vite',
-				'GraphQL',
-				'WebRTC',
+				'Midjourney',
+				'Postgres',
 			],
-			description: 'A platform to build and grow your online business',
-			link: {
-				label: 'consultly.com',
-				href: 'https://consultly.com/',
-			},
-		},
-		{
-			title: 'Monito',
-			techStack: ['Side Project', 'TypeScript', 'Next.js', 'Browser Extension'],
 			description:
-				'Browser extension that records everything happening in a web application',
+				'AI image generator that trains model and generates chonky pets in under 60 seconds',
 			link: {
-				label: 'monito.dev',
-				href: 'https://monito.dev/',
+				label: 'chonkify.vercel.app/',
+				href: 'https://chonkify.vercel.app/',
 			},
-		},
-		{
-			title: 'Jarocki.me',
-			techStack: ['Side Project', 'Next.js', 'MDX'],
-			description:
-				'Personal website and blog. Built with Next.js and Notion API',
-			link: {
-				label: 'github.com',
-				href: 'https://jarocki.me/',
-			},
-		},
-		{
-			title: 'Minimal',
-			techStack: ['Side Project', 'Next.js', 'Puppeteer'],
-			description:
-				'Minimalist calendars, habit trackers, and planners generator',
-			link: {
-				label: 'useminimal.com',
-				href: 'https://useminimal.com/',
-			},
-		},
-		{
-			title: 'Barepapers',
-			techStack: ['Side Project', 'Next.js', 'Puppeteer'],
-			description:
-				'Generates beautiful wallpapers using random shapes and gradients',
-			link: {
-				label: 'barepapers.com',
-				href: 'https://barepapers.com/',
-			},
-		},
-		{
-			title: 'Year progress',
-			techStack: ['Side Project', 'TypeScript', 'Next.js'],
-			description: 'Tracks current year progress and displays a countdown',
-			link: {
-				label: 'getyearprogress.com',
-				href: 'https://getyearprogress.com/',
-			},
-		},
-		{
-			title: 'Mobile Vikings',
-			techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-			description:
-				'Android application for leading virtual mobile operator in Poland',
-			link: {
-				label: 'mobilevikings.pl',
-				href: 'https://mobilevikings.pl/',
-			},
-		},
-		{
-			title: 'Howdy',
-			techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-			description: 'Howdy is a place for joining communities you care about',
-			link: {
-				label: 'play.google.com',
-				href: 'https://howdy.co/',
-			},
-		},
-		{
-			title: 'Tastycloud',
-			techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-			description:
-				'Android application for managing and displaying restaurant menus in kiosk mode',
-			link: {
-				label: 'tastycloud.fr',
-				href: 'https://www.tastycloud.fr/',
-			},
-		},
-		{
-			title: 'Ambit',
-			techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-			description:
-				'Android application that helps with sharing your contact details',
-		},
-		{
-			title: 'Bim',
-			techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-			description:
-				'Android application that helps with booking a table in a restaurants',
-		},
-		{
-			title: 'Canal Digital GO',
-			techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-			description:
-				'Video streaming mobile application for Canal Digital subscribers',
 		},
 	],
 } as const
